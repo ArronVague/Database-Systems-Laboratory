24d3ea0106b120932f3e788deafde63720e36c97
SmileExpression <15018135093@163.com>
2023-11-10 23:17:18 +0800

    Complete sign up

---
+++ lab11-case0604/Frm_SignUp.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace lab11_case0604
+{
+    partial class Frm_SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_RePassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rbtn_Sex1 = new System.Windows.Forms.RadioButton();
+            this.rbtn_Sex2 = new System.Windows.Forms.RadioButton();
+            this.nudown_Age = new System.Windows.Forms.NumericUpDown();
+            this.btn_Register = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Location = new System.Drawing.Point(206, 22);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(164, 21);
+            this.txt_Username.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(206, 75);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.PasswordChar = '*';
+            this.txt_Password.Size = new System.Drawing.Size(164, 21);
+            this.txt_Password.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Confirmed password";
+            // 
+            // txt_RePassword
+            // 
+            this.txt_RePassword.Location = new System.Drawing.Point(206, 132);
+            this.txt_RePassword.Name = "txt_RePassword";
+            this.txt_RePassword.PasswordChar = '*';
+            this.txt_RePassword.Size = new System.Drawing.Size(164, 21);
+            this.txt_RePassword.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Sex";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 242);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Age";
+            // 
+            // rbtn_Sex1
+            // 
+            this.rbtn_Sex1.AutoSize = true;
+            this.rbtn_Sex1.Location = new System.Drawing.Point(206, 189);
+            this.rbtn_Sex1.Name = "rbtn_Sex1";
+            this.rbtn_Sex1.Size = new System.Drawing.Size(47, 16);
+            this.rbtn_Sex1.TabIndex = 12;
+            this.rbtn_Sex1.TabStop = true;
+            this.rbtn_Sex1.Text = "Male";
+            this.rbtn_Sex1.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Sex2
+            // 
+            this.rbtn_Sex2.AutoSize = true;
+            this.rbtn_Sex2.Location = new System.Drawing.Point(307, 189);
+            this.rbtn_Sex2.Name = "rbtn_Sex2";
+            this.rbtn_Sex2.Size = new System.Drawing.Size(59, 16);
+            this.rbtn_Sex2.TabIndex = 13;
+            this.rbtn_Sex2.TabStop = true;
+            this.rbtn_Sex2.Text = "Female";
+            this.rbtn_Sex2.UseVisualStyleBackColor = true;
+            // 
+            // nudown_Age
+            // 
+            this.nudown_Age.Location = new System.Drawing.Point(206, 240);
+            this.nudown_Age.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudown_Age.Name = "nudown_Age";
+            this.nudown_Age.Size = new System.Drawing.Size(120, 21);
+            this.nudown_Age.TabIndex = 14;
+            this.nudown_Age.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(206, 282);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(75, 23);
+            this.btn_Register.TabIndex = 15;
+            this.btn_Register.Text = "Register";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // Frm_SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 317);
+            this.Controls.Add(this.btn_Register);
+            this.Controls.Add(this.nudown_Age);
+            this.Controls.Add(this.rbtn_Sex2);
+            this.Controls.Add(this.rbtn_Sex1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_RePassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Username);
+            this.Name = "Frm_SignUp";
+            this.Text = "Frm_SignUp";
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_RePassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton rbtn_Sex1;
+        private System.Windows.Forms.RadioButton rbtn_Sex2;
+        private System.Windows.Forms.NumericUpDown nudown_Age;
+        private System.Windows.Forms.Button btn_Register;
+    }
+}
---
+++ lab11-case0604/Frm_SignUp.cs
@@ -0,0 +1,95 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab11_case0604
+{
+    public partial class Frm_SignUp : Form
+    {
+        public Frm_SignUp()
+        {
+            InitializeComponent();
+/*            rbtn_Sex1.Checked = true;
+            rbtn_Sex2.Checked = !rbtn_Sex1.Checked;*/
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            // check whether username is null
+            if (txt_Username.Text.Trim().Length == 0)
+            {
+                txt_Username.Focus();
+                return;
+            }
+
+            if (txt_Password.Text.Trim().Length == 0)
+            {
+                txt_Password.Focus(); 
+                return;
+            }
+
+            if (txt_RePassword.Text.Trim().Length == 0)
+            {
+                txt_RePassword.Focus();
+                return;
+            }
+
+            if (txt_Password.Text.Trim() != txt_RePassword.Text.Trim())
+            {
+                MessageBox.Show("The passwords are different!");
+                txt_RePassword.Focus();
+                return;
+            }
+
+            if (!rbtn_Sex1.Checked && !rbtn_Sex2.Checked)
+            {
+                MessageBox.Show("Please select your sex!");
+                return;
+            }
+
+            // check if the username has been used before 
+            string username = txt_Username.Text.Trim();
+            string query = string.Format("SELECT * FROM user_info WHERE UserName='{0}'", username);
+            MySqlConnection conn = Database.GetMySqlConnection();
+            conn.Open();
+            MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conn.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("This username exists!");
+                txt_Username.Focus();
+                return;
+            }
+
+            string password = txt_Password.Text.Trim();
+            int sex = rbtn_Sex1.Checked ? 1 : 2;
+            string age = nudown_Age.Value.ToString();
+
+            query = string.Format("INSERT INTO user_info VALUES(NULL, '{0}', '{1}', {2}, {3})", username, password, age, sex);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            int affected = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (affected > 0)
+            {
+                MessageBox.Show("Register successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Fail!");
+            }
+            return;
+        }
+    }
+}
