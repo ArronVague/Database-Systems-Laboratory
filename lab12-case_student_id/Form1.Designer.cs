9488fe96a824ab45dc83612174e1ff8072ca7d8f
SmileExpression <15018135093@163.com>
2023-11-17 17:44:37 +0800

    Complete form1's UI

---
+++ lab12-case_student_id/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace lab12_case_student_id
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.rbtn_Sex2 = new System.Windows.Forms.RadioButton();
+            this.rbtn_Sex1 = new System.Windows.Forms.RadioButton();
+            this.nudown_Age = new System.Windows.Forms.NumericUpDown();
+            this.txt_Telephone = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Company = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lv_Customer = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbl_Note = new System.Windows.Forms.Label();
+            this.btn_Del = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_Status);
+            this.groupBox1.Controls.Add(this.btn_Cancel);
+            this.groupBox1.Controls.Add(this.btn_Save);
+            this.groupBox1.Controls.Add(this.rbtn_Sex2);
+            this.groupBox1.Controls.Add(this.rbtn_Sex1);
+            this.groupBox1.Controls.Add(this.nudown_Age);
+            this.groupBox1.Controls.Add(this.txt_Telephone);
+            this.groupBox1.Controls.Add(this.txt_Address);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_Company);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_Name);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(731, 371);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edit customer information  Status:";
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Location = new System.Drawing.Point(436, 0);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(82, 24);
+            this.lbl_Status.TabIndex = 17;
+            this.lbl_Status.Text = "label7";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(387, 297);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(101, 45);
+            this.btn_Cancel.TabIndex = 16;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(189, 297);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(101, 45);
+            this.btn_Save.TabIndex = 15;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Sex2
+            // 
+            this.rbtn_Sex2.AutoSize = true;
+            this.rbtn_Sex2.Location = new System.Drawing.Point(245, 157);
+            this.rbtn_Sex2.Name = "rbtn_Sex2";
+            this.rbtn_Sex2.Size = new System.Drawing.Size(113, 28);
+            this.rbtn_Sex2.TabIndex = 14;
+            this.rbtn_Sex2.TabStop = true;
+            this.rbtn_Sex2.Text = "Female";
+            this.rbtn_Sex2.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_Sex1
+            // 
+            this.rbtn_Sex1.AutoSize = true;
+            this.rbtn_Sex1.Location = new System.Drawing.Point(151, 156);
+            this.rbtn_Sex1.Name = "rbtn_Sex1";
+            this.rbtn_Sex1.Size = new System.Drawing.Size(89, 28);
+            this.rbtn_Sex1.TabIndex = 13;
+            this.rbtn_Sex1.TabStop = true;
+            this.rbtn_Sex1.Text = "Male";
+            this.rbtn_Sex1.UseVisualStyleBackColor = true;
+            // 
+            // nudown_Age
+            // 
+            this.nudown_Age.Location = new System.Drawing.Point(502, 150);
+            this.nudown_Age.Name = "nudown_Age";
+            this.nudown_Age.Size = new System.Drawing.Size(120, 35);
+            this.nudown_Age.TabIndex = 12;
+            // 
+            // txt_Telephone
+            // 
+            this.txt_Telephone.Location = new System.Drawing.Point(151, 229);
+            this.txt_Telephone.Name = "txt_Telephone";
+            this.txt_Telephone.Size = new System.Drawing.Size(203, 35);
+            this.txt_Telephone.TabIndex = 11;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(502, 232);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(203, 35);
+            this.txt_Address.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(398, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Age";
+            // 
+            // txt_Company
+            // 
+            this.txt_Company.Location = new System.Drawing.Point(502, 62);
+            this.txt_Company.Name = "txt_Company";
+            this.txt_Company.Size = new System.Drawing.Size(203, 35);
+            this.txt_Company.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Company";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(151, 65);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(203, 35);
+            this.txt_Name.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telephone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sex";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lv_Customer);
+            this.groupBox2.Controls.Add(this.lbl_Note);
+            this.groupBox2.Controls.Add(this.btn_Del);
+            this.groupBox2.Location = new System.Drawing.Point(43, 428);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(731, 498);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Customer information list";
+            // 
+            // lv_Customer
+            // 
+            this.lv_Customer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lv_Customer.FullRowSelect = true;
+            this.lv_Customer.GridLines = true;
+            this.lv_Customer.HideSelection = false;
+            this.lv_Customer.Location = new System.Drawing.Point(21, 35);
+            this.lv_Customer.MultiSelect = false;
+            this.lv_Customer.Name = "lv_Customer";
+            this.lv_Customer.Size = new System.Drawing.Size(684, 457);
+            this.lv_Customer.TabIndex = 19;
+            this.lv_Customer.UseCompatibleStateImageBehavior = false;
+            this.lv_Customer.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Company";
+            this.columnHeader3.Width = 117;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Sex";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Age";
+            this.columnHeader5.Width = 59;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Telephone";
+            this.columnHeader6.Width = 136;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Address";
+            this.columnHeader7.Width = 322;
+            // 
+            // lbl_Note
+            // 
+            this.lbl_Note.AutoSize = true;
+            this.lbl_Note.Location = new System.Drawing.Point(474, 0);
+            this.lbl_Note.Name = "lbl_Note";
+            this.lbl_Note.Size = new System.Drawing.Size(82, 24);
+            this.lbl_Note.TabIndex = 18;
+            this.lbl_Note.Text = "label7";
+            // 
+            // btn_Del
+            // 
+            this.btn_Del.Location = new System.Drawing.Point(322, -12);
+            this.btn_Del.Name = "btn_Del";
+            this.btn_Del.Size = new System.Drawing.Size(122, 49);
+            this.btn_Del.TabIndex = 0;
+            this.btn_Del.Text = "Delete";
+            this.btn_Del.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 949);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudown_Age)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Company;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.RadioButton rbtn_Sex2;
+        private System.Windows.Forms.RadioButton rbtn_Sex1;
+        private System.Windows.Forms.NumericUpDown nudown_Age;
+        private System.Windows.Forms.TextBox txt_Telephone;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Del;
+        private System.Windows.Forms.Label lbl_Note;
+        private System.Windows.Forms.ListView lv_Customer;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+    }
+}
+
