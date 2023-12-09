e510c89c2c94225d7e278fe3e22ecbc71fe7c210
SmileExpression <15018135093@163.com>
2023-12-09 15:34:05 +0800

    Test reader information management's sql

--- lab15-library-management-system/Test/test.sql
+++ lab15-library-management-system/Test/test.sql
@@ -2,4 +2,48 @@
 INSERT INTO
     reader_category
 VALUES
-(NULL, 'test', 10, 10);
+    (NULL, 'test', 10, 10);
+
+SELECT
+    Rid,
+    PASSWORD,
+    reader_category.name AS category,
+    reader_information.name,
+    gender,
+    number,
+    registration,
+    valid,
+    currently,
+    cumulative,
+    loss,
+    violations,
+    remark
+FROM
+    reader_information,
+    reader_category
+WHERE
+    reader_information.`Cid` = reader_category.`Cid`;
+
+INSERT INTO
+    reader_information
+VALUES
+    (
+        5,
+        '123456',
+        2,
+        'test',
+        'male',
+        '12345678901',
+        '2021-01-01',
+        'NULL',
+        0,
+        0,
+        FALSE,
+        0,
+        NULL
+    );
+
+SELECT
+    *
+FROM
+    reader_category
